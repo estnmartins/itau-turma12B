967d0c40a0780793866abf262d22aab712890527
Estevam Nascimento Martins <69018254+estnmartins@users.noreply.github.com>
2020-12-14 12:25:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <h1>Bem vindo turma 12 B itaú</h1>
 
-<? 
+<?php
 
 $i = 1;
 $n = 5;
@@ -9,6 +9,8 @@ $i = $i + 500 - 300;
 $n = $n * 5000;
 
 
-echo "O valor da soma é $i \n";
+echo "O valor da soma é $i\n";
 
 echo "O valor da multiplicaçao é $n";
+
+?>
